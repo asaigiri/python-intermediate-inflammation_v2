9d3f81a9506a3bcc71a0c00571b35aaf68ee5317
Saigiridhari, Anurag <anurag.saigiridhari@ukaea.uk>
2022-05-24 10:34:13 +0100

    min/max additions

--- tests/test_models.py
+++ tests/test_models.py
@@ -37,6 +37,38 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
-    
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+@pytest.mark.parametrize(
+    "test_min, expected_min",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [1, 2]),
+    ])
+def test_daily_min(test_min, expected_min):
+    """Test min function works for array of zeroes and positive integers."""#
+    from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test_min)), np.array(expected_min))
+
+@pytest.mark.parametrize(
+    "test_max, expected_max",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [5, 6]),
+    ])
+def test_daily_max(test_max, expected_max):
+    """Test min function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test_max)), np.array(expected_max))
 
 
